1ee1efeb16b624c765063eb57970aea4f16652cf
Taha  Semlali <idrissistaha@gmail.com>
2026-03-14 22:52:20 +0100

    [CHANGE] Test player turn and setup field entries box collider

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static readonly Vector3 CoinRotation = new Vector3(90, 0, -90);
+
     [Header("Players")] 
     [SerializeField] 
     private GameObject player1;
@@ -12,6 +14,8 @@ public class GameManager : MonoBehaviour
     [Header("Spawn Positions")]
     [SerializeField]
     private GameObject[] spawnPositions;
+    
+    private bool player1Turn = true;
 
     public void SelectColumn(int index)
     {
@@ -21,7 +25,17 @@ public class GameManager : MonoBehaviour
 
     private void TakeTurn(int columnIndex)
     {
-        var coin = Instantiate(player1, spawnPositions[columnIndex].transform.position, Quaternion.identity);
-        coin.transform.Rotate(new Vector3(90, 0, -90));
+        GameObject currentPlayer = player1Turn ? player1 : player2;
+        Vector3 rotation = CoinRotation;
+
+        SpawnCoin(currentPlayer, spawnPositions[columnIndex].transform.position, rotation);
+
+        player1Turn = !player1Turn;
+    }
+
+    private void SpawnCoin(GameObject player, Vector3 position, Vector3 rotation)
+    {
+        var coin = Instantiate(player, position, Quaternion.identity);
+        coin.transform.Rotate(rotation);
     }
 }
