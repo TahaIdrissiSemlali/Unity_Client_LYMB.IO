05f5daccf68306f2fa3ff4dd460074b32ab1d58a
Taha  Semlali <idrissistaha@gmail.com>
2026-03-15 16:22:07 +0100

    [CHANGE] Setup coin drop sound

--- Assets/Scripts/FieldEntry.cs
+++ Assets/Scripts/FieldEntry.cs
@@ -8,10 +8,23 @@ public class FieldEntry : MonoBehaviour
     
     [SerializeField]
     private GameManager gameManager;
+    
+    [Header("Drop Sound")]
+    [SerializeField]
+    private AudioClip dropSound;
+       
+    private AudioSource dropSoundSource;
+
+    private void Start()
+    {
+        dropSoundSource = gameObject.AddComponent<AudioSource>();
+        dropSoundSource.clip = dropSound;
+    }
 
     private void OnMouseDown()
     {
         gameManager.SelectColumn(columIndex);
+        dropSoundSource.Play();
     }
 
     private void OnMouseOver()
